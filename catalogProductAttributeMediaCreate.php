5e51faa59e930669a51b4624f17252323163f4b1
Rupert Jones <rjones@ibuildings.com>
2012-09-10 12:21:22 +0100

    catalogProductAttributeMediaCreate script

---
+++ catalogProductAttributeMediaCreate.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'RemoteService.php';
+
+try {
+    // Get a sales order information by Magento increment ID
+    $file = new stdClass;
+    $file->content = base64_encode(file_get_contents($argv[2]));
+    $file->mime = 'image/jpeg';
+    $file->name = 'Kitten';
+
+    $media = new stdClass;
+    $media->file = $file;
+    $media->label = 'Kitten';
+    $media->position = 1;
+    $media->types = array('small_image', 'image', 'thumbnail');
+    $media->storeView = 'default';
+
+    $service = new RemoteService();
+    $result = $service->catalogProductAttributeMediaCreate(
+        array(
+            'product' => $argv[1],
+            'data' => $media
+        )
+    );
+    print_r($result);
+} catch (SoapFault $e) {
+    echo "Exception '" . get_class($e) ."':\n";
+    print_r($e);
+}
