49f4f840211d398ef87acd87bfe4ae0eed51e798
Rupert Jones <rjones@inviqa.com>
2013-03-06 15:23:40 +0000

    Sales Order Add Comment call

---
+++ src/salesOrderAddComment.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'RemoteService.php';
+
+try {
+    // Get a sales order information by Magento increment ID
+    $service = new RemoteService();
+
+    $orderId = $service->arg(1);
+    $status  = $service->arg(2);
+    $comment = $service->arg(3);
+
+    $result = $service->salesOrderInfo(array('orderIncrementId' => $orderId));
+    if ($result->status == $status) {
+        die("Order {$orderId} is already in state '{$status}'\n");
+    } else {
+        $result = $service->salesOrderAddComment(array(
+            'orderIncrementId' => $orderId,
+            'status'  => $status,
+            'comment' => $comment
+        ));
+        if ($result) {
+            die("Updated {$orderId} to status '{$status}'\n");
+        } else {
+            die("Order {$orderId} failed to update status.\n");
+        }
+    }
+} catch (SoapFault $e) {
+    echo "Exception '" . get_class($e) ."':\n";
+    print_r($e);
+}
