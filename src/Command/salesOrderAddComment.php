3d83e69d6651eea0787bf7ebb8debdfde5741644
Rupert Jones <rjones@inviqa.com>
2015-12-11 11:01:55 +0000

    Add some parameter checks when adding order comment

--- src/Command/salesOrderAddComment.php
+++ src/Command/salesOrderAddComment.php
@@ -9,6 +9,16 @@ try {
     $status  = $service->arg(2);
     $comment = $service->arg(3);
 
+    if (is_null($orderId)) {
+        die("Please provide an order increment ID to set the status for\n");
+    }
+    if (is_null($status)) {
+        die("Please provide a status to set\n");
+    }
+    if (is_null($comment)) {
+        $comment = "Order status set to '{$status}' using API";
+    }
+
     $result = $service->salesOrderInfo(array('orderIncrementId' => $orderId));
     if ($result->result->status == $status) {
         die("Order {$orderId} is already in state '{$status}'\n");
